afa678f2d0435314229f68dd0eb2759a28ec368e
SajjaTulasiKrishna <tulasisajja788@gmail.com>
2026-03-13 15:41:10 +0530

    backend added

---
+++ Backend/Student_Portal_Backend/src/main/java/mits/Student_Portal_Backend/Student_Repo.java
@@ -0,0 +1,10 @@
+package mits.Student_Portal_Backend;
+
+import org.springframework.data.jpa.repository.JpaRepository;	
+
+
+
+public interface Student_Repo extends JpaRepository<Student, Long> {
+
+	Student findByEmail(String email);
+}
